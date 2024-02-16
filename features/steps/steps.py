65eba3d31ef3fc76717a110024d64725b286adb9
Francis <francis.norton@gmail.com>
2024-02-16 11:39:52 +0000

    Added initial table functionality

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,14 +1,29 @@
-from behave import *
-from twentyone import *
+from behave import given, when, then
+from twentyone import Dealer
 
-@given('a dealer')
+@given('a dealer')  
 def step_impl(context):
-    context.dealer = Dealer()
+    context.dealer = Dealer() 
 
-@when('the round starts')
-def step_impl(context):
+@given('a {hand}')  
+def step_impl(context, hand):  # noqa: F811
+    context.dealer = Dealer() 
+    context.dealer.hand = hand.split(',')
+
+@when('the round starts') 
+def step_impl(context):  # noqa: F811
     context.dealer.new_round()
 
-@then('the dealer gives himself two cards')
-def step_impl(context):
+@when('the dealer sums the cards')  
+def step_impl(context):  # noqa: F811
+    context.dealer_total = context.dealer.get_hand_total()
+
+@then('the dealer gives himself two cards') 
+def step_impl(context):  # noqa: F811
     assert (len(context.dealer.hand) == 2)
+
+@then('the {total:d} is correct') 
+def step_impl(context, total):  # noqa: F811
+    assert(context.dealer_total == total)
+
+
